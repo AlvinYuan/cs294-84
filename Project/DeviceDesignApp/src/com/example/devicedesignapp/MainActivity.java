b0221eb004e85078a9c87e903ec2a11ce54bfa13
alvinyuan <alvin.yuan@berkeley.edu>
2013-11-03 23:12:25 -0800

    Prototype android app

---
+++ Project/DeviceDesignApp/src/com/example/devicedesignapp/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.devicedesignapp;
+
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+
+    public void DangerButtonPress(View v) {
+        try {
+            Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+            Ringtone r = RingtoneManager.getRingtone(getApplicationContext(), notification);
+            r.play();
+        } catch (Exception e) {}
+    }
+
+}
