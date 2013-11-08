665ecbb63c88ee6b3ea5573eeaa2c619b25f6ade
alvinyuan <alvin.yuan@berkeley.edu>
2013-11-08 14:06:38 -0800

    AudioSerial receive

--- Project/ServoControl/src/com/audioserial/servocontrol/MainActivity.java
+++ Project/ServoControl/src/com/audioserial/servocontrol/MainActivity.java
@@ -1,63 +1,128 @@
 package com.audioserial.servocontrol;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.app.Activity;
-import android.view.Menu;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
-	Button button1,button2,button3;
-	int servoposition;
-	AudioSerial audioserial;
-	TextView textview;
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		// Opening AudioSerial communication at 20 bits per second and with starting byte 138
-		audioserial=new AudioSerial(20,138);
-//		audioserial.send(0); // Set servo starting position to 0  
-		servoposition=0;
+    private static final int BAUD_RATE = 20;
+    private static final int POLL_INTERVAL = 300;//1000 / BAUD_RATE / 4;
+    private static final int AUDIO_SERIAL_RECEIVE_AMPLITUDE_THRESHOLD = 500;
 
-		textview = (TextView) findViewById(R.id.textView1);
+    Button releaseButton, resetButton;
+    TextView textview, sensorReading, micTextView, doorTextView;
 
-		button1 = (Button) findViewById(R.id.button1);
+    AudioSerial audioserial;
+    boolean stopped;
+    int updateCount = 0;
 
-		button1.setOnClickListener(new OnClickListener() {
-			public void onClick(View v) {
+    private SoundMeter mSensor = new SoundMeter();
+    private Handler mHandler = new Handler();
+    private Runnable mPollTask = new Runnable() {
+        public void run() {
+            int amp = mSensor.getAmplitude();
+            if (amp > AUDIO_SERIAL_RECEIVE_AMPLITUDE_THRESHOLD) {
+                doorTextView.setText("Door is open");
+            } else {
+                doorTextView.setText("Door is closed");
+            }
 
-				if(servoposition<180){
-					servoposition+=1;
-					textview.setText("Servo Position : "+servoposition);
-					audioserial.send('a' + servoposition);
-				}
-			}
-		});
-		button2 = (Button) findViewById(R.id.button2);
-		button2.setOnClickListener(new View.OnClickListener() {
-			public void onClick(View v) {
-				if(servoposition>0){
-					servoposition-=1;
-					textview.setText("Servo Position : "+servoposition);
-					audioserial.send('a' + servoposition);
-				}
-			}
-		});
-		button3 = (Button) findViewById(R.id.button3);
-		button3.setOnClickListener(new View.OnClickListener() {
-			public void onClick(View v) {
-				audioserial=new AudioSerial(20,138);
-//				audioserial.send(0); // Set servo starting position to 0  
-				servoposition=0;
-				textview.setText("Servo Position : "+servoposition);
-				
-			}
-		});
-	}
+            updateCount++;
+            sensorReading.setText("Sensor Reading: " + amp + "\nUpdate " + updateCount);
+            if (!stopped) {
+                mHandler.postDelayed(mPollTask, POLL_INTERVAL);
+            }
+        }
+    };
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
+        textview = (TextView) findViewById(R.id.textView1);
+        sensorReading = (TextView) findViewById(R.id.sensorReadingTextView);
+        micTextView = (TextView) findViewById(R.id.micTextView);
+        doorTextView = (TextView) findViewById(R.id.doorTextView);
 
+        // Opening AudioSerial communication at 20 bits per second and with starting byte 138
+        reset();
+
+        releaseButton = (Button) findViewById(R.id.releaseButton);
+        releaseButton.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+                textview.setText("Congratulations. You came in like a wrecking ball.");
+                audioserial.send('R');
+            }
+        });
+
+        resetButton = (Button) findViewById(R.id.resetButton);
+        resetButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                reset();
+            }
+        });
+}
+
+    //http://stackoverflow.com/questions/14708636/detect-whether-headset-has-microphone
+    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            final String action = intent.getAction();
+            if (Intent.ACTION_HEADSET_PLUG.equals(action)) {
+                Log.d("HeadSetPlugInTest", "state: " + intent.getIntExtra("state", -1));
+                Log.d("HeadSetPlugInTest", "microphone: " + intent.getIntExtra("microphone", -1));
+                if (intent.getIntExtra("state", -1) != 1) {
+                    micTextView.setText("No AUDIO CONNECTION FOUND");
+                } else if (intent.getIntExtra("microphone", -1) == 1) {
+                    micTextView.setText("MIC FOUND");
+                } else {
+                    micTextView.setText("NO MIC FOUND");
+                }
+            }
+        }
+    };
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        IntentFilter filter = new IntentFilter(Intent.ACTION_HEADSET_PLUG);
+        getApplicationContext().registerReceiver(mReceiver, filter);
+
+        stopped = false;
+        start();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        getApplicationContext().unregisterReceiver(mReceiver);
+
+        stopped = true;
+        stop();
+    }
+
+    private void start() {
+        mSensor.start();
+        mHandler.postDelayed(mPollTask, POLL_INTERVAL);
+    }
+
+    private void stop() {
+        mHandler.removeCallbacks(mPollTask);
+        mSensor.stop();
+    }
+
+    void reset() {
+        audioserial=new AudioSerial(BAUD_RATE,138);
+    }
 }
--- Project/audioserial-arduino/audioTest/audioTest.ino
+++ Project/audioserial-arduino/audioTest/audioTest.ino
@@ -2,6 +2,11 @@
 
 /* ServoControl
 *  Receive servo position from audio serial
+* Arduino Pins:
+* A0 on op amp output
+* D2 output to voltage divider (1/2 factor) to capacitor to mic input
+* GND and VCC to audio jack circuit
+* More circuitry for audio jack.
 */
 
 #include <AudioSerial.h>
@@ -11,12 +16,18 @@
 // with starting byte 138 and voltage threshold 30
 AudioSerial audioserial(A0,20,138,30);
 
+const int micOutputPin = 2;
 int previousValue = 0;
 int count = 0;
+
+int incomingByte = 0;
 void setup(){
  Serial.begin(9600);
  Serial.println("hello"); 
+ pinMode(micOutputPin, OUTPUT); 
+ digitalWrite(micOutputPin,LOW);
 }
+
 void loop(){
   audioserial.run(); 
   // Read one byte
@@ -26,6 +37,20 @@ void loop(){
     Serial.println(receivebyte);
     Serial.println();
   } 
+  
+  // send bytes through the microphone
+  if (Serial.available() > 0) {
+          // read the incoming byte:
+          incomingByte = Serial.read();
+          
+          if (incomingByte == 'a'){
+           digitalWrite(micOutputPin,HIGH);
+           Serial.println("you've received a danger message! correct message");
+          }else{            
+            digitalWrite(micOutputPin,LOW);
+          }
+
+  }
 
 // Some Test Code
 //    delay(audioserial.getDelay() / 3);
