5d883ba6c627d003f3bbebb17626e3fe6213d216
alvinyuan <alvin.yuan@berkeley.edu>
2013-11-24 11:26:56 -0800

    cleaned up code organization

--- Project/ServoControl/src/com/audioserial/servocontrol/AudioSerial.java
+++ Project/ServoControl/src/com/audioserial/servocontrol/AudioSerial.java
@@ -9,26 +9,66 @@ package com.audioserial.servocontrol;
 import android.media.AudioFormat;
 import android.media.AudioManager;
 import android.media.AudioTrack;
+import android.os.Handler;
+import android.widget.TextView;
 
 public class AudioSerial {
+    // TX
     public static final int BITS_PER_BYTE = 10;
     public static final char MESSAGE_DELIMITER = '\n';
+    // RX
+    private static final int POLL_INTERVAL = 500;// every half second
 
+    // GENERAL
+    int baudrate = 300;
+    int sampleRate = 44100;
+
+    // TX
     AudioTrack audiotrack;
     short[] low;
     short[] high;
     short[] endTrack;
 
-    int baudrate;
-    int sampleRate;
-    int bitlength;
-    int bufferSize;
+    // RX
+    boolean stopped = true;
+    int updateCount = 0;
+    TextView sensorReading;
+    SoundMeter mSensor = new SoundMeter();
+    Handler mHandler = new Handler();
+    Runnable mPollTask = new Runnable() {
+        public void run() {
+            int amp = mSensor.getAmplitude();
 
-    public AudioSerial(int baudrate, int sampleRate){
+            if (sensorReading != null) {
+                updateCount++;
+                sensorReading.setText("Sensor Reading: " + amp + "\nUpdate " + updateCount);
+            }
+
+            if (!stopped) {
+                mHandler.postDelayed(mPollTask, POLL_INTERVAL);
+            }
+        }
+    };
+
+    // GENERAL METHODS
+    public void reset(int baudrate, int sampleRate) {
         this.baudrate = baudrate;
         this.sampleRate = sampleRate;
-        bitlength=sampleRate/baudrate;
-        bufferSize = AudioTrack.getMinBufferSize(sampleRate, AudioFormat.CHANNEL_OUT_MONO, AudioFormat.ENCODING_PCM_16BIT);
+        resetTX();
+    }
+
+    private int bitlength() {
+        return sampleRate/baudrate;
+    }
+
+    // TX METHODS
+    private int TXbufferSize() {
+        return AudioTrack.getMinBufferSize(sampleRate, AudioFormat.CHANNEL_OUT_MONO, AudioFormat.ENCODING_PCM_16BIT);
+    }
+
+    private void resetTX() {
+        int bitlength = bitlength();
+        int TXbufferSize = TXbufferSize();
 
         // Initialization of low and high waveforms
         low=new short[bitlength];
@@ -38,29 +78,33 @@ public class AudioSerial {
             high[i]= Short.MAX_VALUE;
         }
         // Initialization of endTrack buffer
-        endTrack = new short[bufferSize];
-        for (int i = 0; i < bufferSize; i++) {
+        endTrack = new short[TXbufferSize];
+        for (int i = 0; i < TXbufferSize; i++) {
             endTrack[i] = 0;
         }
 
-        audiotrack= new AudioTrack(AudioManager.STREAM_MUSIC, sampleRate, AudioFormat.CHANNEL_OUT_MONO, AudioFormat.ENCODING_PCM_16BIT, bufferSize, AudioTrack.MODE_STREAM);
+        audiotrack= new AudioTrack(AudioManager.STREAM_MUSIC, sampleRate, AudioFormat.CHANNEL_OUT_MONO, AudioFormat.ENCODING_PCM_16BIT, TXbufferSize, AudioTrack.MODE_STREAM);
         audiotrack.setStereoVolume(1,1);
         audiotrack.play();
     }
 
     // BUG: If the first bit sent after the first start bit is LOW, you can get a weird read from the microcontroller.
     public void send(String str, boolean isInverted) {
+        int TXbufferSize = TXbufferSize();
+
         for (int i = 0; i < str.length(); i++) {
             send(str.charAt(i), isInverted);
         }
         send(MESSAGE_DELIMITER, isInverted);
 
         // endTrack: This ensures all important audiotrack data is played.
-        audiotrack.write(endTrack, 0, bufferSize);
+        audiotrack.write(endTrack, 0, TXbufferSize);
     }
 
     // Sending one byte starting from the least significant bit
     public void send(int val, boolean isInverted) {
+        int bitlength = bitlength();
+
         // Start bit
         audiotrack.write(high, 0, bitlength);
 
@@ -77,4 +121,26 @@ public class AudioSerial {
         // Stop bit
         audiotrack.write(low, 0, bitlength);
     }
+
+    // RX METHODS
+    public void resetRX() {
+
+    }
+
+    public void startRX() {
+        if (stopped) {
+            stopped = false;
+            mSensor.start();
+            mHandler.postDelayed(mPollTask, POLL_INTERVAL);
+        }
+    }
+
+    public void stopRX() {
+        if (!stopped) {
+            stopped = true;
+            mHandler.removeCallbacks(mPollTask);
+            mSensor.stop();
+        }
+    }
+
 }
--- Project/ServoControl/src/com/audioserial/servocontrol/MainActivity.java
+++ Project/ServoControl/src/com/audioserial/servocontrol/MainActivity.java
@@ -3,7 +3,6 @@ package com.audioserial.servocontrol;
 import android.media.AudioFormat;
 import android.media.AudioTrack;
 import android.os.Bundle;
-import android.os.Handler;
 import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
@@ -16,29 +15,10 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
-    private static final int POLL_INTERVAL = 3000;//1000 / BAUD_RATE / 4;
-    private static final int AUDIO_SERIAL_RECEIVE_AMPLITUDE_THRESHOLD = 300;
-
     Button dangerButton, sosButton, resetButton;
     TextView sensorReading, micTextView, baudRateEditText, sampleRateEditText, bufferSizeTextView;
 
-    AudioSerial audioserial;
-    boolean stopped;
-    int updateCount = 0;
-
-    private SoundMeter mSensor = new SoundMeter();
-    private Handler mHandler = new Handler();
-    private Runnable mPollTask = new Runnable() {
-        public void run() {
-            int amp = mSensor.getAmplitude();
-
-            updateCount++;
-            sensorReading.setText("Sensor Reading: " + amp + "\nUpdate " + updateCount);
-            if (!stopped) {
-                mHandler.postDelayed(mPollTask, POLL_INTERVAL);
-            }
-        }
-    };
+    AudioSerial audioserial = new AudioSerial();;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -77,6 +57,37 @@ public class MainActivity extends Activity {
         });
 }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        // For determining whether audio jack / microphone is plugged into phone.
+        IntentFilter filter = new IntentFilter(Intent.ACTION_HEADSET_PLUG);
+        getApplicationContext().registerReceiver(mReceiver, filter);
+
+        audioserial.startRX();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        getApplicationContext().unregisterReceiver(mReceiver);
+
+        audioserial.stopRX();
+    }
+
+    void reset() {
+        int baudRate = Integer.parseInt(baudRateEditText.getText().toString());
+        int sampleRate = Integer.parseInt(sampleRateEditText.getText().toString());
+        int bufferSize = AudioTrack.getMinBufferSize(sampleRate, AudioFormat.CHANNEL_OUT_MONO, AudioFormat.ENCODING_PCM_16BIT);
+        if (bufferSize > 0) {
+            bufferSizeTextView.setText("Min Buffer Size = " + bufferSize);
+            audioserial.reset(baudRate, sampleRate);
+            audioserial.sensorReading = sensorReading;
+        } else {
+            bufferSizeTextView.setText("Got invalid buffer size");
+        }
+    }
+
     //http://stackoverflow.com/questions/14708636/detect-whether-headset-has-microphone
     private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
         @Override
@@ -96,44 +107,4 @@ public class MainActivity extends Activity {
         }
     };
 
-    @Override
-    public void onResume() {
-        super.onResume();
-        IntentFilter filter = new IntentFilter(Intent.ACTION_HEADSET_PLUG);
-        getApplicationContext().registerReceiver(mReceiver, filter);
-
-        stopped = false;
-        start();
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-        getApplicationContext().unregisterReceiver(mReceiver);
-
-        stopped = true;
-        stop();
-    }
-
-    private void start() {
-        mSensor.start();
-        mHandler.postDelayed(mPollTask, POLL_INTERVAL);
-    }
-
-    private void stop() {
-        mHandler.removeCallbacks(mPollTask);
-        mSensor.stop();
-    }
-
-    void reset() {
-        int baudRate = Integer.parseInt(baudRateEditText.getText().toString());
-        int sampleRate = Integer.parseInt(sampleRateEditText.getText().toString());
-        int bufferSize = AudioTrack.getMinBufferSize(sampleRate, AudioFormat.CHANNEL_OUT_MONO, AudioFormat.ENCODING_PCM_16BIT);
-        if (bufferSize > 0) {
-            bufferSizeTextView.setText("Min Buffer Size = " + bufferSize);
-            audioserial=new AudioSerial(baudRate, sampleRate);
-        } else {
-            bufferSizeTextView.setText("Got invalid buffer size");
-        }
-    }
 }
