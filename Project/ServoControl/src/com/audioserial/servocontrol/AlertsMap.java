589b5c2dfa9d29c0703b0adf42fa88f8cdb69264
Lanssie Mingyue Ma <l.mao.ftwin@gmail.com>
2013-11-24 14:50:32 -0800

    created alerts map page

---
+++ Project/ServoControl/src/com/audioserial/servocontrol/AlertsMap.java
@@ -0,0 +1,22 @@
+package com.audioserial.servocontrol;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class AlertsMap extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_alerts_map);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.alerts_map, menu);
+		return true;
+	}
+
+}
