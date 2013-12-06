38c8e8b382306c44b65ade27cc0be1d7ab7fa74c
Lanssie Mingyue Ma <l.mao.ftwin@gmail.com>
2013-12-06 11:59:40 -0800

    mobile app checked Radio ID added to string msg

--- Project/ServoControl/src/com/audioserial/servocontrol/Send_message_actvity.java
+++ Project/ServoControl/src/com/audioserial/servocontrol/Send_message_actvity.java
@@ -17,8 +17,9 @@ import android.widget.RadioGroup.OnCheckedChangeListener;
 public class Send_message_actvity extends Activity {
     TextView details;
     int seekbarProgress = 0;
+    String dangerChecked = " ";
     Button sendMessageButton, seeAlertsLog, seeAlertsMap;
-    Button fire, eq, flood, chem, gun, other;
+//    Button fire, eq, flood, chem, gun, other;
     RadioGroup g1, g2;
 
     @Override
@@ -27,18 +28,26 @@ public class Send_message_actvity extends Activity {
         setContentView(R.layout.activity_send_message_actvity);
         details = (EditText) findViewById(R.id.Details);
 
-        fire = (Button) findViewById(R.id.fire);
-        eq = (Button) findViewById(R.id.eq);
-        flood = (Button) findViewById(R.id.flood);
-        chem = (Button) findViewById(R.id.chem);
-        gun = (Button) findViewById(R.id.gun);
-        other = (Button) findViewById(R.id.other);
+//        fire = (Button) findViewById(R.id.fire);
+//        eq = (Button) findViewById(R.id.eq);
+//        flood = (Button) findViewById(R.id.flood);
+//        chem = (Button) findViewById(R.id.chem);
+//        gun = (Button) findViewById(R.id.gun);
+//        other = (Button) findViewById(R.id.other);
         //what the danger is, fire, flood?
         g1 = (RadioGroup) findViewById(R.id.radioGroup1);
         g1.setOnCheckedChangeListener(new OnCheckedChangeListener() {
 
             public void onCheckedChanged(RadioGroup group, int checkedId) {
-//                s += checkedId + " ";
+            	if (checkedId == R.id.fire) {
+            		dangerChecked = "fire" + " ";
+            	}
+            	else if (checkedId == R.id.eq){
+            		dangerChecked = "eq" + " ";
+            	}
+            	else if (checkedId == R.id.gun){
+            		dangerChecked = "gun" + " ";
+            	}
             }
         });
 
@@ -46,7 +55,15 @@ public class Send_message_actvity extends Activity {
         g2.setOnCheckedChangeListener(new OnCheckedChangeListener() {
 
             public void onCheckedChanged(RadioGroup group, int checkedId) {
-//                s += checkedId + " ";
+            	if (checkedId == R.id.chem) {
+            		dangerChecked = "chem" + " ";
+            	}
+            	else if (checkedId == R.id.flood){
+            		dangerChecked = "flood" + " ";
+            	}
+            	else if (checkedId == R.id.other){
+            		dangerChecked = "other" + " ";
+            	}
             }
         });
 
@@ -70,30 +87,26 @@ public class Send_message_actvity extends Activity {
                 // TODO Auto-generated method stub
             }
         });
-
+        
         Switch sos = (Switch) findViewById(R.id.SOS);
         sos.setOnClickListener(new OnClickListener() {
 
             public void onClick(View v){
-                AudioSerial.singleton.send(" SOS /n", true);
+                AudioSerial.singleton.send(" SOS \n", true);
             }
         });
 
         sendMessageButton = (Button) findViewById(R.id.SendMsg);
         sendMessageButton.setOnClickListener(new OnClickListener() {
 
-        public void onClick(View v) {
-                String f = "D ";
-                f += seekbarProgress + " ";
-                f += details.getText().toString();
+        	public void onClick(View v) {
+                String f = "D "+ dangerChecked + " " + seekbarProgress + " " + details.getText().toString() + " ";
                 f += AudioSerial.MESSAGE_DELIMITER;
                 System.out.println(f);
                 AudioSerial.singleton.send(f, true);
             }
         });
-
     }
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
